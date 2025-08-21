e9466a5cf8819e5dcfb8289f057e4eb8ac160227
yazeedmomani <yazeed.02.momani@outlook.com>
2025-08-21 20:48:37 +0300

    add get_user method in database

--- src/db/Database.java
+++ src/db/Database.java
@@ -1,12 +1,39 @@
 package db;
 
+import db.models.User;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class Database {
     private static final String URL = "jdbc:sqlite:src/db/budget.db";
 
     public static Connection get_connection() throws Exception{
         return DriverManager.getConnection(URL);
+    }
+
+    public static User get_user(String username, String password) throws Exception{
+        String sql = "SELECT * FROM users WHERE username = ? AND password = ?";
+
+        try(Connection connection = Database.get_connection()){
+            PreparedStatement template = connection.prepareStatement(sql);
+
+            template.setString(1, username);
+            template.setString(2, password);
+
+            try(ResultSet result = template.executeQuery()){
+                if(result.next()){
+                    return new User(
+                            result.getInt("id"),
+                            result.getString("name"),
+                            result.getString("username"), 
+                            result.getString("password")
+                    );
+                }
+                return null;
+            }
+        }
     }
 }
