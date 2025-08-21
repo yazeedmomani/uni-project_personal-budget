c08be58c8b0fc46dfd7f0200444eee3f00e7f031
yazeedmomani <yazeed.02.momani@outlook.com>
2025-08-21 21:39:23 +0300

    Organize code

--- src/db/Database.java
+++ src/db/Database.java
@@ -1,5 +1,6 @@
 package db;
 
+import db.dao.UsersDAO;
 import db.models.User;
 
 import java.sql.Connection;
@@ -14,41 +15,12 @@ public class Database {
         return DriverManager.getConnection(URL);
     }
 
+    // Users
     public static User get_user(String username, String password) throws Exception{
-        String sql = "SELECT * FROM users WHERE username = ? AND password = ?";
-
-        try(Connection connection = Database.get_connection()){
-            PreparedStatement template = connection.prepareStatement(sql);
-
-            template.setString(1, username);
-            template.setString(2, password);
-
-            try(ResultSet result = template.executeQuery()){
-                if(result.next()){
-                    return new User(
-                            result.getInt("id"),
-                            result.getString("name"),
-                            result.getString("username"),
-                            result.getString("password")
-                    );
-                }
-                return null;
-            }
-        }
+        return UsersDAO.get(username, password);
     }
 
     public static void update_user(User user) throws Exception{
-        String sql = "UPDATE users SET name = ?, username = ?, password = ? WHERE id = ?";
-
-        try(Connection connection = Database.get_connection()){
-            PreparedStatement template = connection.prepareStatement(sql);
-
-            template.setString(1, user.getName());
-            template.setString(2, user.getUsername());
-            template.setString(3, user.getPassword());
-            template.setInt(4, user.getId());
-
-            template.executeUpdate();
-        }
+        UsersDAO.update(user);
     }
 }
---
+++ src/db/dao/UsersDAO.java
@@ -0,0 +1,48 @@
+package db.dao;
+
+import db.Database;
+import db.models.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class UsersDAO {
+    public static User get(String username, String password) throws Exception{
+        String sql = "SELECT * FROM users WHERE username = ? AND password = ?";
+
+        try(Connection connection = Database.get_connection()){
+            PreparedStatement template = connection.prepareStatement(sql);
+
+            template.setString(1, username);
+            template.setString(2, password);
+
+            try(ResultSet result = template.executeQuery()){
+                if(result.next()){
+                    return new User(
+                            result.getInt("id"),
+                            result.getString("name"),
+                            result.getString("username"),
+                            result.getString("password")
+                    );
+                }
+                return null;
+            }
+        }
+    }
+
+    public static void update(User user) throws Exception{
+        String sql = "UPDATE users SET name = ?, username = ?, password = ? WHERE id = ?";
+
+        try(Connection connection = Database.get_connection()){
+            PreparedStatement template = connection.prepareStatement(sql);
+
+            template.setString(1, user.getName());
+            template.setString(2, user.getUsername());
+            template.setString(3, user.getPassword());
+            template.setInt(4, user.getId());
+
+            template.executeUpdate();
+        }
+    }
+}
