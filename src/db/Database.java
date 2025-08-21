f245132079774dff4fe62fd0b51e3507e4ed3d32
yazeedmomani <yazeed.02.momani@outlook.com>
2025-08-21 20:28:16 +0300

    Create Database.java

--- src/db/Database.java
+++ src/db/Database.java
@@ -1,5 +1,12 @@
 package db;
 
-public class Database {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class Database {
+    private static final String URL = "jdbc:sqlite:src/db/budget.db";
+
+    public static Connection get_connection() throws Exception{
+        return DriverManager.getConnection(URL);
+    }
 }
