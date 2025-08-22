0b975228be372bdb8e12ab533131500f6f4da207
yazeedmomani <yazeed.02.momani@outlook.com>
2025-08-22 03:38:27 +0300

    Encapsulate DAOs

--- src/db/Database.java
+++ src/db/Database.java
@@ -1,5 +1,7 @@
 package db;
 
+import db.dao.IncomeDAO;
+import db.dao.SavingsDAO;
 import db.dao.UsersDAO;
 import db.models.User;
 
@@ -12,6 +14,10 @@ import java.time.format.DateTimeFormatter;
 public class Database {
     private static final String URL = "jdbc:sqlite:src/db/budget.db";
     private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    private static User currentUser;
+
+    public static IncomeDAO incomeLog;
+    public static SavingsDAO savingsLog;
 
     public static Connection getConnection() throws Exception{
         return DriverManager.getConnection(URL);
@@ -26,6 +32,21 @@ public class Database {
     public static void updateUser(User user) throws Exception{
         UsersDAO.update(user);
     }
+
+    public static void setCurrentUser(User user){
+        if(user == null){
+            currentUser = user;
+            incomeLog = null;
+            savingsLog = null;
+            return;
+        }
+        currentUser = user;
+        incomeLog = new IncomeDAO(user.getId());
+        savingsLog = new SavingsDAO(user.getId());
+    }
+
+    public static User getCurrentUser() {return currentUser;}
+
 
     // Date Format
     public static DateTimeFormatter getDateFormat() {return DATE_FORMAT;}
