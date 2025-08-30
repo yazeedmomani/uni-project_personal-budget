a52a484f877cb47ef17a361c19ac563e27edf22b
yazeedmomani <yazeed.02.momani@outlook.com>
2025-08-31 00:41:28 +0300

    Create receivables edit

--- src/layout/views/debts_receivables/DebtsReceivablesEdit.java
+++ src/layout/views/debts_receivables/DebtsReceivablesEdit.java
@@ -2,65 +2,66 @@ package layout.views.debts_receivables;
 
 import db.Database;
 import db.Validator;
-import db.dao.IncomeDAO;
-import db.models.IncomeRecord;
+import db.dao.DebtsDAO;
+import db.models.DebtsRecord;
 import javafx.scene.control.TextField;
 import layout.views.templates.TemplateEdit;
 
 import java.time.LocalDate;
 
-public class DebtsReceivablesEdit extends TemplateEdit<IncomeRecord, IncomeDAO> {
-    private TextField dateField, sourceField, amountField;
+public class DebtsReceivablesEdit extends TemplateEdit<DebtsRecord, DebtsDAO> {
+    private TextField dateField, partyField, amountField;
+    private final String DEBT_TYPE = "Credited";
 
     public DebtsReceivablesEdit(){
-        super(Database.getIncomeDAO());
+        super(Database.getDebtsDAO());
     }
 
     protected void initializeCustomFields(){
         dateField = form.addField("Date", "YYYY-MM-DD");
-        sourceField = form.addField("Source", "Income Source");
+        partyField = form.addField("Source", "Income Source");
         amountField = form.addField("Amount", "0.00");
 
         dateField.setOnKeyPressed(super::eventHandler);
-        sourceField.setOnKeyPressed(super::eventHandler);
+        partyField.setOnKeyPressed(super::eventHandler);
         amountField.setOnKeyPressed(super::eventHandler);
     }
 
     protected void setFieldTextToRetrievedValue(){
         dateField.setText(record.getDate().toString());
-        sourceField.setText(record.getSource());
+        partyField.setText(record.getParty());
         amountField.setText(String.valueOf(record.getAmount()));
     }
 
     protected boolean checkSourceEqualAnyFields(Object source){
-        return source.equals(dateField) || source.equals(sourceField) || source.equals(amountField);
+        return source.equals(dateField) || source.equals(partyField) || source.equals(amountField);
     }
 
     protected boolean assertAndGetFromFieldsAndSetCurrentRecord(){
-        if (validator.assertNotEmpty(dateField, sourceField, amountField)) return true;
+        if (validator.assertNotEmpty(dateField, partyField, amountField)) return true;
         if (validator.assertDateFormat(dateField)) return true;
         if (validator.assertPositiveNumber(amountField)) return true;
 
         LocalDate date = Validator.getLocalDate(dateField);
-        String source = Validator.getString(sourceField);
+        String party = Validator.getString(partyField);
         double amount = Validator.getDouble(amountField);
 
         record.setDate(date);
-        record.setSource(source);
+        record.setParty(party);
         record.setAmount(amount);
         return false;
     }
 
     protected boolean assertAndGetFromFieldsAndCreateNewRecord(String notes){
-        if (validator.assertNotEmpty(dateField, sourceField, amountField)) return true;
+        if (validator.assertNotEmpty(dateField, partyField, amountField)) return true;
         if (validator.assertDateFormat(dateField)) return true;
         if (validator.assertPositiveNumber(amountField)) return true;
 
         LocalDate date = Validator.getLocalDate(dateField);
-        String source = Validator.getString(sourceField);
+        String party = Validator.getString(partyField);
         double amount = Validator.getDouble(amountField);
 
-        record = new IncomeRecord(date, source, amount, notes);
+        record = new DebtsRecord(date, party, amount, notes, DEBT_TYPE);
         return false;
     }
 }
